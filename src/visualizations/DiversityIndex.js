b41283e7315ef576725f35a8b6ba308d554c5ca9
Zac Ioannidis <zacoppotamus@gmail.com>
2020-03-06 15:19:56 +0200

    Rendering points using BufferGeometry; initializing/resetting/updating force layout

--- src/visualizations/DiversityIndex.js
+++ src/visualizations/DiversityIndex.js
@@ -1,12 +1,14 @@
 import * as THREE from "three";
 
 import { clamp } from "../utils";
+import pointVS from "../shaders/point.vs";
+import pointFS from "../shaders/point.fs";
 import ForceLayout from "../workers/subscribers/force-layout-links";
 import Renderer2D from "./Renderer2D";
 import theme from "../styles";
 
 class DiversityIndex extends Renderer2D {
-  constructor({ canvas, hudCanvas }) {
+  constructor({ canvas, hudCanvas, drawSecondCanvas = true }) {
     super({ canvas });
 
     this.groups = {
@@ -14,14 +16,21 @@ class DiversityIndex extends Renderer2D {
     };
 
     // get access to HUD canvas context
-    this.hudCanvas = hudCanvas;
-    this.hudCanvas.width = this.width;
-    this.hudCanvas.height = this.height;
-    this.ctx = hudCanvas.getContext("2d");
-    this.ctx.scale(window.devicePixelRatio || 1, window.devicePixelRatio || 1);
+    this.drawSecondCanvas = drawSecondCanvas;
+    if (this.drawSecondCanvas) {
+      this.hudCanvas = hudCanvas;
+      this.hudCanvas.width = this.width;
+      this.hudCanvas.height = this.height;
+      this.ctx = hudCanvas.getContext("2d");
+      this.ctx.scale(
+        window.devicePixelRatio || 1,
+        window.devicePixelRatio || 1
+      );
+    }
 
     this.scene.add(this.groups.points);
 
+    this.initForceLayout();
     this.initScrollListeners();
     this.animate = this.animate.bind(this);
     this.animate();
@@ -40,23 +49,29 @@ class DiversityIndex extends Renderer2D {
       y:
         this.scales.category(this.scales.groupFunc(d)) +
         this.scales.y(this.scales.yFunc(d)),
-      r: Math.random() * 5
+      r: Math.random() * 10 + 2
     }));
-    const {
-      set: simulationSet,
-      tick: tickSimulation,
-      tickS,
-      terminate
-    } = ForceLayout({
-      nodes: this.nodes,
+
+    this.updateForceLayout(this.nodes);
+
+    if (this.drawSecondCanvas) {
+      this.drawLabels();
+    }
+    this.draw();
+  }
+
+  initForceLayout() {
+    this.forceLayout = ForceLayout({
+      nodes: [],
       links: []
     });
+  }
 
-    this.tickS = tickS;
-    this.tickSimulation = tickSimulation;
-    this.terminate && this.terminate();
-    this.terminate = terminate;
-    this.simulationSet = simulationSet;
+  updateForceLayout(nodes) {
+    this.forceLayout.elapsedTicks = 0;
+    this.forceLayout.set("nodes", nodes);
+    // this.forceLayout.start();
+    // this.forceLayout.tick(100);
   }
 
   setLayout(layout) {
@@ -88,14 +103,21 @@ class DiversityIndex extends Renderer2D {
     this.camera.bottom = yOffset + this.height;
     this.camera.updateProjectionMatrix();
 
-    // scroll HUD canvas
-    const { a: scaleX, d: scaleY } = this.ctx.getTransform();
-    this.ctx.setTransform(scaleX, 0, 0, scaleY, 0, 0);
-    this.ctx.clearRect(0, 0, this.hudCanvas.width, this.hudCanvas.height);
-    this.ctx.translate(0, -yOffset / scaleY);
-    this.drawLabels();
+    if (this.drawSecondCanvas) {
+      // scroll HUD canvas
+      const { a: scaleX, d: scaleY } = this.ctx.getTransform();
+      this.ctx.setTransform(scaleX, 0, 0, scaleY, 0, 0);
+      this.ctx.clearRect(0, 0, this.hudCanvas.width, this.hudCanvas.height);
+      this.ctx.translate(0, -yOffset / scaleY);
+      this.drawLabels();
+    }
 
     // @todo (optional) if simulation is running do something funky with vy
+  }
+
+  // clean up
+  destroy() {
+    this.forceLayout.terminate();
   }
 
   drawLabels() {
@@ -116,39 +138,73 @@ class DiversityIndex extends Renderer2D {
   }
 
   draw() {
+    // EXIT previous
     for (var i = this.groups.points.children.length - 1; i >= 0; i--) {
       this.groups.points.remove(this.groups.points.children[i]);
     }
 
-    let pointsGeometry = new THREE.Geometry();
+    this.geometry = new THREE.BufferGeometry();
+    this.attributes = {
+      position: [],
+      color: [],
+      size: [],
+      opacity: []
+    };
 
-    let colors = [];
+    var color = new THREE.Color();
 
-    this.data.forEach(d => {
-      pointsGeometry.vertices.push(
-        new THREE.Vector3(
-          this.scales.x(this.scales.xFunc(d)),
-          this.scales.category(this.scales.groupFunc(d)) +
-            this.scales.y(this.scales.yFunc(d)),
-          0
-        )
-      );
-      colors.push(new THREE.Color(0xffffff));
+    for (let i = 0; i < this.nodes.length; i++) {
+      const { x, y, r } = this.nodes[i];
+      color.setRGB(1, 1, 1);
+
+      this.attributes.position.push(x, y, 0);
+      this.attributes.color.push(color.r, color.g, color.b);
+      this.attributes.size.push(2 * r);
+      this.attributes.opacity.push(1);
+    }
+
+    this.geometry.setAttribute(
+      "position",
+      new THREE.Float32BufferAttribute(this.attributes.position, 3)
+    );
+
+    this.geometry.setAttribute(
+      "customColor",
+      new THREE.Float32BufferAttribute(this.attributes.color, 3)
+    );
+
+    this.geometry.setAttribute(
+      "size",
+      new THREE.Float32BufferAttribute(this.attributes.size, 1)
+    );
+
+    this.geometry.setAttribute(
+      "opacity",
+      new THREE.Float32BufferAttribute(this.attributes.opacity, 1)
+    );
+
+    this.geometry.computeBoundingSphere();
+
+    this.material = new THREE.ShaderMaterial({
+      uniforms: {
+        color: {
+          value: new THREE.Color(0xffffff)
+        },
+        noDepth: {
+          value: true
+        }
+      },
+
+      vertexShader: pointVS,
+      fragmentShader: pointFS,
+
+      blending: THREE.AdditiveBlending,
+      depthTest: false,
+      transparent: true
     });
-    pointsGeometry.colors = colors;
-
-    let pointsMaterial = new THREE.PointsMaterial({
-      size: 2,
-      sizeAttenuation: false,
-      vertexColors: THREE.VertexColors
-    });
-
-    const points = new THREE.Points(pointsGeometry, pointsMaterial);
-
+    const points = new THREE.Points(this.geometry, this.material);
     this.groups.points.add(points);
     this.groups.points.position.x = this.width * 0.2;
-    // this.scene.add(this.groups.points);
-
     this.bbox = {
       max: new THREE.Box3().setFromObject(this.scene).max,
       min: new THREE.Box3().setFromObject(this.scene).min
@@ -157,18 +213,23 @@ class DiversityIndex extends Renderer2D {
   }
 
   update() {
-    if (this.tickSimulation) {
-      this.tickSimulation(1).then(({ nodes }) => {
+    if (this.forceLayout && this.forceLayout.elapsedTicks < 120) {
+      this.forceLayout.tick(1).then(({ nodes }) => {
+        this.forceLayout.elapsedTicks++;
         this.nodes = nodes;
         // update positions / radii
-        const vertices = this.groups.points.children[0].geometry.vertices;
-        for (var i = 0; i < vertices.length; i++) {
-          vertices[i].x = nodes[i].x;
-          vertices[i].y = nodes[i].y;
+        const positions = this.groups.points.children[0].geometry.attributes
+          .position.array;
+
+        for (var i = 0; i < nodes.length; i++) {
+          const i3 = i * 3;
+          positions[i3] = nodes[i].x;
+          positions[i3 + 1] = nodes[i].y;
         }
+        this.groups.points.children[0].geometry.attributes.position.needsUpdate = true;
 
         // @todo use morph target
-        this.groups.points.children[0].geometry.verticesNeedUpdate = true;
+        // this.groups.points.children[0].geometry.verticesNeedUpdate = true;
       });
     }
   }
