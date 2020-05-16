08bb4ea89da22c3ed2b5c071daeb8af3ced9ece5
Zac Ioannidis <zacoppotamus@gmail.com>
2020-05-16 17:43:29 +0300

    Code splitting of cached data

--- src/OrionData.context.js
+++ src/OrionData.context.js
@@ -14,10 +14,10 @@ import { AbsoluteCanvas, initApp } from "./components/shared/renderer";
 import { accessors } from "../src/utils";
 import LoadingBar from "./components/shared/loading-bar";
 import { SEED_DATA } from "./queries";
-// import cachedData from "./data/data.json";
 import { ParticleContainerLatentSpace } from "./visualizations/LatentSpace";
 import { DiversityIndex } from "./visualizations/DiversityIndex";
 import CrossFilter from "./workers/subscribers/crossfilter";
+import { useEffect } from "react";
 
 const OrionDataContext = createContext({});
 
@@ -121,33 +121,40 @@ const LoadingOrChildren = ({ ready, children, data }) => {
 };
 
 // eslint-disable-next-line
-// const FetchOffline = ({ children }) => {
-//   const data = useRef();
-//   const { byCountry, byTopic, byYear } = cachedData.data;
-//   data.current = cachedData.data;
-//   data.current.papers = {
-//     byCountry,
-//     byTopic,
-//     byYear,
-//   };
+const FetchOffline = ({ children }) => {
+  const [cachedDataLoaded, setCachedDataLoaded] = useState(false);
+  const data = useRef();
+  useEffect(() => {
+    import("./data/data.json").then((cachedData) => {
+      const { byCountry, byTopic, byYear } = cachedData.data;
+      data.current = cachedData.data;
+      data.current.papers = {
+        byCountry,
+        byTopic,
+        byYear,
+      };
 
-//   delete data.current.byCountry;
-//   delete data.current.byTopic;
-//   delete data.current.byYear;
+      delete data.current.byCountry;
+      delete data.current.byTopic;
+      delete data.current.byYear;
 
-//   return (
-//     <LoadingOrChildren ready={true} data={data.current}>
-//       {children}
-//     </LoadingOrChildren>
-//   );
-// };
+      setCachedDataLoaded(true);
+    });
+  }, []);
+
+  return cachedDataLoaded ? (
+    <LoadingOrChildren ready={true} data={data.current}>
+      {children}
+    </LoadingOrChildren>
+  ) : null;
+};
 
 export const OrionDataProvider = ({ children }) => {
   return (
     <>
       {process.env.NODE_ENV === "development" && (
-        <FetchOnline>{children}</FetchOnline>
-        // <FetchOffline>{children}</FetchOffline>
+        // <FetchOnline>{children}</FetchOnline>
+        <FetchOffline>{children}</FetchOffline>
       )}
       {process.env.NODE_ENV === "production" && (
         <FetchOnline>{children}</FetchOnline>
